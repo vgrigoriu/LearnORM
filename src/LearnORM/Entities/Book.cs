159283f89cfd228cf4e222067edd29ca251b3c0a
Victor Grigoriu <vgrigoriu@gmail.com>
2014-08-23 12:06:59 +0300

    Create Book entity

---
+++ src/LearnORM/Entities/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities
+{
+    public class Book : BaseEntity
+    {
+        public string Title { get; set; }
+        public string TitleForSorting { get; set; }
+        public string OriginalTitle { get; set; }
+    }
+}
