2425e4fe6f94ff1b5dadb42a0f539f2dd30205fb
Victor Grigoriu <vgrigoriu@gmail.com>
2014-08-23 12:14:48 +0300

    Add Book migration

---
+++ src/LearnORM/DatabaseMigrations/CreateBookTable.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace DatabaseMigrations
+{
+    [Migration(201408231208, "Create Book table")]
+    public class CreateBookTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Book")
+                .WithDescription("Represents a book")
+                .InSchema("dbo")
+                .WithColumn("Id").AsInt32().PrimaryKey("PK_Book")
+                .WithColumn("Title").AsString(255)
+                .WithColumn("TitleForSorting").AsString(255)
+                .WithColumn("OriginalTitle").AsString(255);
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Book");
+        }
+    }
+}
