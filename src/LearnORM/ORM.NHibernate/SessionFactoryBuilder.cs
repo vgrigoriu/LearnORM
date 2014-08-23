b88bdeebd4c93413ea9f42b5b3a1c13cc17d2e68
Victor Grigoriu <vgrigoriu@gmail.com>
2014-08-23 11:53:58 +0300

    Add NHibernate config project

---
+++ src/LearnORM/ORM.NHibernate/SessionFactoryBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Dialect;
+using NHibernate.Mapping.ByCode;
+
+namespace ORM.NHibernate
+{
+    public class SessionFactoryBuilder
+    {
+        public ISessionFactory Build()
+        {
+            var configuration = new Configuration();
+            configuration.DataBaseIntegration(db =>
+            {
+                db.Dialect<MsSql2012Dialect>();
+                db.ConnectionString = "Server=(localdb)\v11.0;Integrated Security=true;";
+            });
+
+            foreach (var mapping in GetMappings())
+            {
+                Trace.WriteLine(mapping.AsString());
+                configuration.AddMapping(mapping);
+            }
+
+            return configuration.BuildSessionFactory();
+        }
+
+        private IEnumerable<HbmMapping> GetMappings()
+        {
+            var mapper = new ConventionModelMapper();
+            return mapper.CompileMappingForEach(GetEntityTypes());
+        }
+
+        private IEnumerable<Type> GetEntityTypes()
+        {
+            return Enumerable.Empty<Type>();
+        }
+    }
+}
