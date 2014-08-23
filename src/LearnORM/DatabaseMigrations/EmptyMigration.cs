799443de01718afaba1db900d501634910152ad2
Victor Grigoriu <vgrigoriu@gmail.com>
2014-08-23 11:22:36 +0300

    Add migrations project

---
+++ src/LearnORM/DatabaseMigrations/EmptyMigration.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace DatabaseMigrations
+{
+    [Migration(201408231121, "Empty migration")]
+    public class EmptyMigration : Migration
+    {
+        public override void Up()
+        {
+            // Empty migration, to be able to revert to an empty DB
+        }
+
+        public override void Down()
+        {
+            // Empty migration, to be able to revert to an empty DB
+        }
+    }
+}
