e4bec33702f78e95e4253de5e7fc11eae3ddfcea
Victor Grigoriu <vgrigoriu@gmail.com>
2014-08-23 12:30:28 +0300

    Add NHibernate test project

---
+++ src/LearnORM/ORM.NHibernate.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ORM.NHibernate.Tests")]
+[assembly: AssemblyDescription("Tests for the NHibernate configuration")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Victor Grigoriu")]
+[assembly: AssemblyProduct("ORM.NHibernate.Tests")]
+[assembly: AssemblyCopyright("Copyright © Victor Grigoriu 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e42ed88b-382d-4731-8681-8b4741b8667d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/LearnORM/ORM.NHibernate.Tests/TrivialTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace ORM.NHibernate.Tests
+{
+    public class TrivialTests
+    {
+        [Fact]
+        public void SanityCheck()
+        {
+            Assert.Equal(2, 1 + 1);
+        } 
+    }
+}
