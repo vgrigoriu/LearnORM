afeb5436d9f8ef707a70ed31dbfa7319fbe63c27
Victor Grigoriu <vgrigoriu@gmail.com>
2014-08-23 12:23:02 +0300

    Make Book columns nullable

---
+++ src/LearnORM/DatabaseMigrations/MakeBookColumnsNullable.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace DatabaseMigrations
+{
+    [Migration(201408221218, "Make Book columns nullable")]
+    public class MakeBookColumnsNullable : Migration
+    {
+        public override void Up()
+        {
+            Alter.Column("TitleForSorting").OnTable("Book").InSchema("dbo")
+                .AsString(255).Nullable();
+            Alter.Column("OriginalTitle").OnTable("Book").InSchema("dbo")
+                .AsString(255).Nullable();
+        }
+
+        public override void Down()
+        {
+            // do nothing, it's ok to leave the nullability as is
+        }
+    }
+}
