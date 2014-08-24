c919068b5b87338da8800bb7fd36c0b87ca81b19
Victor Grigoriu <vgrigoriu@gmail.com>
2014-08-24 16:05:54 +0300

    Create Question, Answer Tables

---
+++ src/LearnORM/DatabaseMigrations/CreateTableAnswer.cs
@@ -0,0 +1,33 @@
+﻿using FluentMigrator;
+
+namespace DatabaseMigrations
+{
+    [Migration(201408241601, "Create table Answer")]
+    public class CreateTableAnswer : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Answer").WithDescription("Represents a quiz answer")
+                .WithColumn("Id").AsInt32().PrimaryKey("PK_Answer")
+                .WithColumn("Text").AsString()
+                .WithColumn("IsCorrect").AsBoolean()
+                .WithColumn("QuestionId").AsInt32().ForeignKey("FK_Answer_Question", "Question", "Id");
+
+            Insert.IntoTable("NextHighValues").Row(new
+            {
+                NextHigh = 0,
+                EntityName = "answer"
+            });
+        }
+
+        public override void Down()
+        {
+            Delete.FromTable("NextHighValues").Row(new
+            {
+                EntityName = "answer"
+            });
+
+            Delete.Table("Answer");
+        }
+    }
+}
---
+++ src/LearnORM/DatabaseMigrations/CreateTableQuestion.cs
@@ -0,0 +1,31 @@
+﻿using FluentMigrator;
+
+namespace DatabaseMigrations
+{
+    [Migration(201408241558, "Create table Question")]
+    public class CreateTableQuestion : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Question").WithDescription("Represents a quiz question")
+                .WithColumn("Id").AsInt32().PrimaryKey("PK_Question")
+                .WithColumn("Text").AsString();
+
+            Insert.IntoTable("NextHighValues").Row(new
+            {
+                NextHigh = 0,
+                EntityName = "question"
+            });
+        }
+
+        public override void Down()
+        {
+            Delete.FromTable("NextHighValues").Row(new
+            {
+                EntityName = "question"
+            });
+
+            Delete.Table("Question");
+        }
+    }
+}
