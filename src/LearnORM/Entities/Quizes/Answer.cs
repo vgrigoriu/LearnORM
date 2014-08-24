8c70b0250c505387de0d6503245e2fd5bb049d85
Victor Grigoriu <vgrigoriu@gmail.com>
2014-08-24 15:57:14 +0300

    Add Question, Answer entities

---
+++ src/LearnORM/Entities/Quizes/Answer.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities.Quizes
+{
+    public class Answer : BaseEntity
+    {
+        public virtual string Text { get; set; }
+        public virtual bool IsCorrect { get; set; }
+    }
+}
---
+++ src/LearnORM/Entities/Quizes/Question.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Entities.Quizes
+{
+    class Question
+    {
+        public virtual string Text { get; set; }
+        public virtual ICollection<Answer> Answers { get; set; }
+    }
+}
