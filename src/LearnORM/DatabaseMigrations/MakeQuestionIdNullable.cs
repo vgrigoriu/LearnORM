824247081d5733a131f2181bbd6d0369c3d56d94
Victor Grigoriu <vgrigoriu@gmail.com>
2014-08-24 17:09:06 +0300

    Test inserting entity with one to many (bag)
    - FK column needs to be nullable

---
+++ src/LearnORM/DatabaseMigrations/MakeQuestionIdNullable.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace DatabaseMigrations
+{
+    [Migration(201408241704, "Make Book.QuestionId nullable")]
+    public class MakeQuestionIdNullable : Migration
+    {
+        public override void Up()
+        {
+            Alter.Column("QuestionId").OnTable("Answer")
+                .AsInt32().Nullable();
+        }
+
+        public override void Down()
+        {
+            // do nothing, it's ok to leave the nullability as is
+        }
+    }
+}
---
+++ src/LearnORM/ORM.NHibernate.Tests/QuestionSavingTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using Entities.Quizes;
+using NHibernate;
+using Xunit;
+
+namespace ORM.NHibernate.Tests
+{
+    public class QuestionSavingTests
+    {
+        [Fact]
+        public void CanSaveQuestionsToMsSql()
+        {
+            var sessionFactoryBuilder = new SessionFactoryBuilder();
+            var sessionFactory = sessionFactoryBuilder.BuildForMsSql();
+
+            CanSaveQuestions(sessionFactory);
+        }
+
+        [Fact]
+        public void CanSaveQuestionsToMySql()
+        {
+            var sessionFactoryBuilder = new SessionFactoryBuilder();
+            var sessionFactory = sessionFactoryBuilder.BuildForMySql();
+
+            CanSaveQuestions(sessionFactory);
+        }
+
+        private void CanSaveQuestions(ISessionFactory sessionFactory)
+        {
+            var questionText = "Can you touch this?_" + Guid.NewGuid();
+            const string correctAnswerText = "No";
+            const string incorrectAnswerText = "Yes";
+
+            int questionId;
+
+            using (var session = sessionFactory.OpenSession())
+            using (var transaction = session.BeginTransaction())
+            {
+                var question = new Question()
+                {
+                    Text = questionText,
+                    Answers = new[]
+                    {
+                        new Answer()
+                        {
+                            Text = correctAnswerText,
+                            IsCorrect = true
+                        },
+                        new Answer()
+                        {
+                            Text = incorrectAnswerText,
+                            IsCorrect = false
+                        }
+                    }
+                };
+
+                session.Save(question);
+
+                transaction.Commit();
+
+                questionId = question.Id;
+            }
+
+            using (var session = sessionFactory.OpenSession())
+            using (var transaction = session.BeginTransaction())
+            {
+                var question = session.Get<Question>(questionId);
+
+                var correctAnswer = question.Answers.Single(a => a.IsCorrect);
+                var incorrectAnswer = question.Answers.Single(a => !a.IsCorrect);
+
+                Assert.Equal(correctAnswerText, correctAnswer.Text);
+                Assert.Equal(incorrectAnswerText, incorrectAnswer.Text);
+
+                transaction.Commit();
+            }
+        }
+    }
+}
