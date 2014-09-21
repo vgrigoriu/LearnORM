98fa550fc0f518246dbdc8f3baf60242f667bee3
Victor Grigoriu <vgrigoriu@gmail.com>
2014-09-21 09:53:41 +0300

    Fixes #10

--- src/LearnORM/ORM.NHibernate.Tests/TestingInfrastructure/DatabaseFactAttribute.cs
+++ src/LearnORM/ORM.NHibernate.Tests/TestingInfrastructure/DatabaseFactAttribute.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using NHibernate;
 using Xunit;
 using Xunit.Sdk;
 
@@ -6,10 +8,17 @@ namespace ORM.NHibernate.Tests.TestingInfrastructure
 {
     public class DatabaseFactAttribute : FactAttribute
     {
+        private static readonly SessionFactoryBuilder SessionFactoryBuilder = new SessionFactoryBuilder();
+
+        private readonly Lazy<ISessionFactory> mySqlSessionFactory = new Lazy<ISessionFactory>(
+            () => SessionFactoryBuilder.BuildForMySql());
+        private readonly Lazy<ISessionFactory> msSqlSessionFactory = new Lazy<ISessionFactory>(
+            () => SessionFactoryBuilder.BuildForMsSql());
+
         protected override IEnumerable<ITestCommand> EnumerateTestCommands(IMethodInfo method)
         {
-            yield return new DatabaseCommand(method, new SessionFactoryBuilder().BuildForMySql(), "MySQL");
-            yield return new DatabaseCommand(method, new SessionFactoryBuilder().BuildForMsSql(), "Microsoft SQL");
+            yield return new DatabaseCommand(method, mySqlSessionFactory.Value, "MySQL");
+            yield return new DatabaseCommand(method, msSqlSessionFactory.Value, "Microsoft SQL");
         }
     }
 }
